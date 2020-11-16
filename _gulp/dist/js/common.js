e86654ae3febe2d2b2ed3fb19bf47f062076bb8e
Dmitriy Khristoev <haossut@gmail.com>
2020-11-16 17:50:34 +0200

    промежуточный коммит

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
   });
 
   formPrayer();
+  formLetter();
 
   // Chrome Smooth Scroll
   try {
@@ -44,6 +45,45 @@ function formPrayer() {
         </div>
         <div class="form-group">
             <input class="form-control --new" type="text" name="last-name-${formArray.length}" placeholder="Фамилия ${formArray.length}">
+        </div>
+    </div>`
+    formWrapper.append(block);
+  });  
+}
+
+function formLetter() {
+  var form = $('.letter-order__add'),
+      btnAddLetter = form.find('.letter-order__add-btn'),
+      letter = $('.letter-order__add-block'),
+      formWrapper = form.find('.letter-order__add-wrapper'),
+      formArray = [0]
+  ;
+
+  $(document).on('click', '.letter-order__block-remove', function() {
+    formArray.pop();
+    $(this).parent().remove();
+  });
+
+  btnAddLetter.on('click', function(){
+    var number = formArray.length - 1;
+    formArray.push(number + 1);
+    var block = `<div class="letter-order__add-block">
+        <button class="letter-order__block-remove" type="button">Удалить</button>
+        <div class="letter-order__block-title">Буква <span>${formArray.length}</span> для</div>
+        <div class="letter-order__block-wrapper">
+            <div class="div1">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="letter-name-${formArray.length}" placeholder="Ваше имя">
+                </div>
+            </div>
+            <div class="div2">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="letter-lastname-${formArray.length}" placeholder="Имя матери">
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="text" name="letter-mother-${formArray.length}" placeholder="Фамилия">
         </div>
     </div>`
     formWrapper.append(block);
